c8a194aa57b000f90cd27392f627174857aed610
Diab Neiroukh <lazerl0rd@thezest.dev>
2021-01-12 21:40:32 +0000

    power: smb-lib: Guard pointer which is only used when debug logbuffer is enabled.
    This fixes the following warning:
    
    ../drivers/power/supply/qcom/smb-lib.c:3426:22: warning: unused variable 'chg' [-Wunused-variable]
            struct smb_charger *chg = irq_data->parent_data;
                                ^
    
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -3423,7 +3423,9 @@ int smblib_get_prop_slave_current_now(struct smb_charger *chg,
 irqreturn_t smblib_handle_debug(int irq, void *data)
 {
 	struct smb_irq_data *irq_data = data;
+#if 0
 	struct smb_charger *chg = irq_data->parent_data;
+#endif
 
 	smblib_dbg(chg, PR_INTERRUPT, "IRQ: %s\n", irq_data->name);
 	return IRQ_HANDLED;
